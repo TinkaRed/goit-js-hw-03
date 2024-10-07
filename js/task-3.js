9a01c412e02164f5d90416382156de7f63f9feec
tinkared <tinkared@gmail.com>
2024-10-07 14:33:56 +0200

    fixed

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,11 @@
 function filterArray(numbers, value) {
-    const filteredNumbers = [];
+    const newNumbers = [];
         for (let i = 0; i < numbers.length; i++) {
             if (numbers[i] > value) {
-            filteredNumbers.push(numbers[i]);
+            newNumbers.push(numbers[i]);
             }
 }
-    return filteredNumbers;
+    return newNumbers;
 }
 console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
 console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
